47169ca61a8cbddb15efb8a82a24ee3c314f3c38
nixos <nixos@server.com>
2023-12-09 16:12:58 +0800

    feat: flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    systems.url = "github:nix-systems/x86_64-linux";
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-23.11;
+  };
+
+  outputs =
+    inputs @ { self
+    , nixpkgs
+    , ...
+    }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      nixosConfigurations."nixos-server" = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = {
+          inherit inputs;
+        };
+        modules =
+          [
+            ./configuration.nix
+          ];
+      };
+    };
+}
